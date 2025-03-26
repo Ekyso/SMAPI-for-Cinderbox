Ekyso/SMAPI-for-Cinderbox

commit f8280292d8f5b4fd45d529217484f10efed93be7

add workaround for game's Data/ChairTiles logic not handling tilesheet asset names containing a mod ID correctly

src/SMAPI/Framework/ContentManagers/ModContentManager.cs
@@ -419,15 +419,21 @@ internal sealed class ModContentManager : BaseContentManager
             // load best match
             try
             {
-                if (!this.TryGetTilesheetAssetName(relativeMapFolder, imageSource, out IAssetName? assetName, out string? error))
+                if (!this.TryGetTilesheetAssetName(relativeMapFolder, imageSource, out IAssetName? assetName, out bool isInternalAssetKey, out string? error))
                     throw new SContentLoadException(ContentLoadErrorType.InvalidData, $"{errorPrefix} {error}");
 
                 if (assetName is not null)
                 {
-                    if (!assetName.IsEquivalentTo(tilesheet.ImageSource))
-                        this.Monitor.VerboseLog($"   Mapped tilesheet '{tilesheet.ImageSource}' to '{assetName}'.");
+                    // Some game code is hardcoded to expect a file extension, which results in issues like the
+                    // `Data/ChairTiles` key for `Author.ModName_Tilesheet` being `Author`.
+                    string newImageSource = !isInternalAssetKey && Path.GetExtension(assetName.Name).ToLowerInvariant() is not (".xnb" or ".png")
+                        ? assetName.Name + ".xnb"
+                        : assetName.Name;
 
-                    tilesheet.ImageSource = assetName.Name;
+                    // set path
+                    if (this.Monitor.IsVerbose && PathUtilities.NormalizeAssetName(tilesheet.ImageSource) != PathUtilities.NormalizePath(newImageSource))
+                        this.Monitor.VerboseLog($"   Mapped tilesheet '{tilesheet.ImageSource}' to '{assetName}'.");
+                    tilesheet.ImageSource = newImageSource;
                 }
             }
             catch (Exception ex)
@@ -444,10 +450,11 @@ internal sealed class ModContentManager : BaseContentManager
     /// <param name="modRelativeMapFolder">The folder path containing the map, relative to the mod folder.</param>
     /// <param name="relativePath">The tilesheet path to load.</param>
     /// <param name="assetName">The found asset name.</param>
+    /// <param name="isInternalAssetKey">Whether the <paramref name="assetName"/> is an internal asset key which points to a file in the mod folder, rather than an asset name in the game's normal content pipeline.</param>
     /// <param name="error">A message indicating why the file couldn't be loaded.</param>
     /// <returns>Returns whether the asset name was found.</returns>
     /// <remarks>See remarks on <see cref="FixTilesheetPaths"/>.</remarks>
-    private bool TryGetTilesheetAssetName(string modRelativeMapFolder, string relativePath, out IAssetName? assetName, out string? error)
+    private bool TryGetTilesheetAssetName(string modRelativeMapFolder, string relativePath, out IAssetName? assetName, out bool isInternalAssetKey, out string? error)
     {
         error = null;
 
@@ -455,6 +462,7 @@ internal sealed class ModContentManager : BaseContentManager
         if (string.IsNullOrWhiteSpace(relativePath))
         {
             assetName = null;
+            isInternalAssetKey = false;
             return true;
         }
 
@@ -473,6 +481,7 @@ internal sealed class ModContentManager : BaseContentManager
             if (this.GetModFile<Texture2D>(localKey).Exists)
             {
                 assetName = this.GetInternalAssetKey(localKey);
+                isInternalAssetKey = true;
                 return true;
             }
         }
@@ -483,6 +492,7 @@ internal sealed class ModContentManager : BaseContentManager
         {
             this.GameContentManager.LoadLocalized<Texture2D>(contentKey, this.GameContentManager.Language, useCache: true); // no need to bypass cache here, since we're not storing the asset
             assetName = contentKey;
+            isInternalAssetKey = false;
             return true;
         }
         catch
@@ -500,6 +510,7 @@ internal sealed class ModContentManager : BaseContentManager
 
         // not found
         assetName = null;
+        isInternalAssetKey = false;
         error = "The tilesheet couldn't be found relative to either the map file or the game's content folder.";
         return false;
     }

commit 8b2e087024c1aca1b38a6104a075e70957eaf26f

revert workaround for game's Data/ChairTiles logic not handling mod IDs in tilesheet names correctly
This reverts commit f8280292d8f5b4fd45d529217484f10efed93be7, which caused crashes loading map tiles in some cases.

src/SMAPI/Framework/ContentManagers/ModContentManager.cs
@@ -419,21 +419,15 @@ internal sealed class ModContentManager : BaseContentManager
             // load best match
             try
             {
-                if (!this.TryGetTilesheetAssetName(relativeMapFolder, imageSource, out IAssetName? assetName, out bool isInternalAssetKey, out string? error))
+                if (!this.TryGetTilesheetAssetName(relativeMapFolder, imageSource, out IAssetName? assetName, out string? error))
                     throw new SContentLoadException(ContentLoadErrorType.InvalidData, $"{errorPrefix} {error}");
 
                 if (assetName is not null)
                 {
-                    // Some game code is hardcoded to expect a file extension, which results in issues like the
-                    // `Data/ChairTiles` key for `Author.ModName_Tilesheet` being `Author`.
-                    string newImageSource = !isInternalAssetKey && Path.GetExtension(assetName.Name).ToLowerInvariant() is not (".xnb" or ".png")
-                        ? assetName.Name + ".xnb"
-                        : assetName.Name;
-
-                    // set path
-                    if (this.Monitor.IsVerbose && PathUtilities.NormalizeAssetName(tilesheet.ImageSource) != PathUtilities.NormalizePath(newImageSource))
+                    if (!assetName.IsEquivalentTo(tilesheet.ImageSource))
                         this.Monitor.VerboseLog($"   Mapped tilesheet '{tilesheet.ImageSource}' to '{assetName}'.");
-                    tilesheet.ImageSource = newImageSource;
+
+                    tilesheet.ImageSource = assetName.Name;
                 }
             }
             catch (Exception ex)
@@ -450,11 +444,10 @@ internal sealed class ModContentManager : BaseContentManager
     /// <param name="modRelativeMapFolder">The folder path containing the map, relative to the mod folder.</param>
     /// <param name="relativePath">The tilesheet path to load.</param>
     /// <param name="assetName">The found asset name.</param>
-    /// <param name="isInternalAssetKey">Whether the <paramref name="assetName"/> is an internal asset key which points to a file in the mod folder, rather than an asset name in the game's normal content pipeline.</param>
     /// <param name="error">A message indicating why the file couldn't be loaded.</param>
     /// <returns>Returns whether the asset name was found.</returns>
     /// <remarks>See remarks on <see cref="FixTilesheetPaths"/>.</remarks>
-    private bool TryGetTilesheetAssetName(string modRelativeMapFolder, string relativePath, out IAssetName? assetName, out bool isInternalAssetKey, out string? error)
+    private bool TryGetTilesheetAssetName(string modRelativeMapFolder, string relativePath, out IAssetName? assetName, out string? error)
     {
         error = null;
 
@@ -462,7 +455,6 @@ internal sealed class ModContentManager : BaseContentManager
         if (string.IsNullOrWhiteSpace(relativePath))
         {
             assetName = null;
-            isInternalAssetKey = false;
             return true;
         }
 
@@ -481,7 +473,6 @@ internal sealed class ModContentManager : BaseContentManager
             if (this.GetModFile<Texture2D>(localKey).Exists)
             {
                 assetName = this.GetInternalAssetKey(localKey);
-                isInternalAssetKey = true;
                 return true;
             }
         }
@@ -492,7 +483,6 @@ internal sealed class ModContentManager : BaseContentManager
         {
             this.GameContentManager.LoadLocalized<Texture2D>(contentKey, this.GameContentManager.Language, useCache: true); // no need to bypass cache here, since we're not storing the asset
             assetName = contentKey;
-            isInternalAssetKey = false;
             return true;
         }
         catch
@@ -510,7 +500,6 @@ internal sealed class ModContentManager : BaseContentManager
 
         // not found
         assetName = null;
-        isInternalAssetKey = false;
         error = "The tilesheet couldn't be found relative to either the map file or the game's content folder.";
         return false;
     }